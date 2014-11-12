12ce7f50f64c847ac56e92580f75bcefdbfcf0fc
Stefan Rinke <sker65@gmail.com>
2014-11-12 22:17:52 +0100

    pins for mega

--- Funhouse.ino
+++ Funhouse.ino
@@ -10,11 +10,14 @@
 
 // init fast led data struct for strip
 #define NUM_LEDS 6
-#define DATA_PIN 13
+#define RGB_DATA_PIN 28
+#define TOPPER1 30
+#define TOPPER2 32
+
 CRGB leds[NUM_LEDS];
 
 // create tasks for actors (lamps, topper, selenoids)
-Topper topper( 300, 6, 7 );
+Topper topper( 300, TOPPER1, TOPPER2 );
 
 Led upperScoop( 2, 4 );
 Led lowerScoop( 0, 2 );
@@ -24,7 +27,7 @@ Attract attract( &topper, &upperScoop, &lowerScoop );
 void setup() {
 
     // init strip
-    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+    FastLED.addLeds<NEOPIXEL, RGB_DATA_PIN>(leds, NUM_LEDS);
     
     // register tasks with scheduler
     SoftTimer.add( &topper );
