89aa6b7c2a7bad989a12abb7393e1c93c23308a0
Stefan Rinke <sker65@gmail.com>
2018-05-24 21:22:49 +0200

    - latest version of light

--- Funhouse.ino
+++ Funhouse.ino
@@ -9,14 +9,23 @@
 #include "ScanSwitchedLamp.h"
 #include "SwitchScanner.h"
 #include "TogglePinAction.h"
+#include "Blink.h"
 
 // init fast led data struct for strip
 #define NUM_LEDS 6
 #define RGB_DATA_PIN 34
 #define TOPPER1 30
 #define TOPPER2 32
+#define SMALL_LOWER_CLOWN 48
+#define UPPER_LEFT_CLOWN 50
+#define UPPER_RIGHT_CLOWN 52
 
 CRGB leds[NUM_LEDS];
+
+Blink b1(SMALL_LOWER_CLOWN, 1200, 1000);
+Blink b2(UPPER_LEFT_CLOWN, 2500, 1200);
+Blink b3(UPPER_RIGHT_CLOWN, 3800, 1400);
+
 
 // create tasks for actors (lamps, topper, selenoids)
 Topper topper( 300, TOPPER1, TOPPER2 );
@@ -26,7 +35,7 @@ Led lowerScoop( 0, 2 );
 
 Attract attract( &topper, &upperScoop, &lowerScoop );
 
-TogglePinAction clown1(13,true);
+TogglePinAction clown1(50,true);
 
 SwitchScanner switchScanner;
 
@@ -45,6 +54,9 @@ void setup() {
     SoftTimer.add( &upperScoop );
     SoftTimer.add( &lowerScoop );
     SoftTimer.add( &switchScanner );
+    SoftTimer.add( &b1 );
+    SoftTimer.add( &b2 );
+    SoftTimer.add( &b3 );
 
     // let topper blink
     topper.setMode( blink50 );
